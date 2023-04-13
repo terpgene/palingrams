753a42174803c8ff3bc67d584b6f6a7844f03de2
dev2me <eugene2essel@gmail.com>
2023-04-13 12:58:10 -0400

    word list and load func

---
+++ load_dictionary.py
@@ -0,0 +1,17 @@
+"""Load a text file as a list.
+
+Arguments:
+-text file name (and directory path, if needed)
+Exceptions:
+-IOError if filename not found
+
+Returns:
+-A list of all words in a text file in lower case
+
+Requires-import sys 
+
+"""
+
+import sys
+def load(file):
+    """Open a text file & return a list of lower case strings."""
