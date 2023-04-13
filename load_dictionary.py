d588f60f0b1c19909e012840a271f7b170a39f10
dev2me <eugene2essel@gmail.com>
2023-04-13 13:05:20 -0400

    fix load func

--- load_dictionary.py
+++ load_dictionary.py
@@ -15,3 +15,15 @@ Requires-import sys
 import sys
 def load(file):
     """Open a text file & return a list of lower case strings."""
+    try:
+        with open(file, mode="r", encoding="utf-8") as in_file:
+            loaded_txt = in_file.read().strip().split('\n')
+            loaded_txt = [x.lower() for x in loaded_txt]
+            return loaded_txt
+    except IOError as error:
+        print(f"{error}\nError opening {file}. Terminating program.", file=sys.stderr)
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    load("words.txt")
